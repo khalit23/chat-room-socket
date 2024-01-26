f1bdfb068d93262d5da1728ce3458af7a57a1a27
khalit23 <khalitjibril23@hotmail.com>
2024-01-26 13:27:05 +0000

    create the function that will start the server

--- server.py
+++ server.py
@@ -1,2 +1,13 @@
 import socket
 import threading
+
+PORT = 5050
+HOSTNAME = socket.gethostname()
+IP = socket.gethostbyname(HOSTNAME)
+ADDRESS = (IP, PORT)
+
+def start_server(address: tuple):
+    pass
+
+if __name__ == "__main__":
+    start_server(ADDRESS)
