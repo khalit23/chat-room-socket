c75bce6c19af0ae324a9bba3cc5fdf15d376e19c
khalit23 <khalitjibril23@hotmail.com>
2024-01-26 17:36:13 +0000

    Create logic for client to send message to the server

--- client.py
+++ client.py
@@ -5,3 +5,13 @@ PORT = 5050
 IP = socket.gethostbyname(HOSTNAME)
 ADDRESS = (IP, PORT)
 CLIENT_SERVER = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+CLIENT_SERVER.connect(ADDRESS)
+FORMAT = "utf-8"
+
+while True:
+    client_message = input("Enter a message you'd like to send: ")
+    if client_message.lower() == "close":
+        CLIENT_SERVER.close()
+        break
+    else:
+        CLIENT_SERVER.send(client_message.encode(FORMAT))
