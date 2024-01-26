dc5575da8713d7488b5698ed3ba65948924f77ab
khalit23 <khalitjibril23@hotmail.com>
2024-01-26 13:35:07 +0000

    create the function that starts the server and awaits a connection from a client

--- server.py
+++ server.py
@@ -7,7 +7,12 @@ IP = socket.gethostbyname(HOSTNAME)
 ADDRESS = (IP, PORT)
 
 def start_server(address: tuple):
-    pass
+    server = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    server.bind(address)
+    server.listen()
+    while True:
+        print(f"[SERVER] is starting ...")
+        client_socket, client_address = server.accept()
 
 if __name__ == "__main__":
     start_server(ADDRESS)
