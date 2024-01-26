c46c0fe71d3169a22721a5700b7c906e04029140
khalit23 <khalitjibril23@hotmail.com>
2024-01-26 17:35:10 +0000

    Create function that handles client joining the server

--- server.py
+++ server.py
@@ -5,17 +5,24 @@ PORT = 5050
 HOSTNAME = socket.gethostname()
 IP = socket.gethostbyname(HOSTNAME)
 ADDRESS = (IP, PORT)
+FORMAT = "utf-8"
 
 def handle_client(client_socket: socket.socket, client_address: tuple):
-    pass
+    byte_message = client_socket.recv(1024)
+    msg = byte_message.decode(FORMAT)
+    print(f"client: {client_address} has sent the message: {msg}")
 
 def start_server(address: tuple):
     server = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
     server.bind(address)
     server.listen()
+    print(f"[SERVER] is listening ...")
     while True:
-        print(f"[SERVER] is starting ...")
         client_socket, client_address = server.accept()
+        print(f"client: {client_address} has joined")
+        thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+        thread.start()
+        print(f"Active number of clients is: {threading.active_count() - 1}")
 
 if __name__ == "__main__":
     start_server(ADDRESS)
