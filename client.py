cabf3d77f8790848311d96d3e76e1ef7ffacf0a4
khalit <khalit@2simple.com>
2024-02-05 10:34:32 +0000

    Allow clients to pick a name and send messages continously to the server

--- client.py
+++ client.py
@@ -8,9 +8,18 @@ CLIENT_SERVER = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 CLIENT_SERVER.connect(ADDRESS)
 FORMAT = "utf-8"
 
-while True:
-    client_message = input("Enter a message you'd like to send: ")
-    CLIENT_SERVER.send(client_message.encode(FORMAT))
-    if client_message.lower() == 'close':
-        CLIENT_SERVER.close()
-        break
+def client_introduce():
+    client_name = input("Please enter your name: ")
+    CLIENT_SERVER.send(client_name.encode(FORMAT))
+
+def send_messages():
+    while True:
+        client_message = input("Enter a message you'd like to send: ")
+        CLIENT_SERVER.send(client_message.encode(FORMAT))
+        if client_message.lower() == 'close':
+            CLIENT_SERVER.close()
+            break
+
+if __name__ == "__main__":
+    client_introduce()
+    send_messages()
