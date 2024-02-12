b1f9432c03c8b69c6ffc7c085a91a7d5b4485ea3
khalitjibril23@hotmail.com <khalitjibril23@hotmail.com>
2024-02-12 20:11:54 +0000

    Put receive message into seperate thread

--- client.py
+++ client.py
@@ -11,27 +11,25 @@ def client_introduce(client_server: socket.socket):
     client_name = input("Please enter your name: ")
     client_server.send(client_name.encode(FORMAT))
 
-def send_messages(client_server: socket.socket):
-    while True:
-        client_message = input("Enter a message you'd like to send: ")
-        client_server.send(client_message.encode(FORMAT))
-        if client_message.lower() == 'close':
-            client_server.close()
-            break
-
 def receive_messages(client_server: socket.socket):
-    while True:
-        try:
-            received_message = client_server.recv(1024).decode(FORMAT)
-            print(received_message)
-        except client_server.timeout:
-            break
+    try:
+        received_message = client_server.recv(1024).decode(FORMAT)
+        print("\n" + received_message)
+    except ConnectionAbortedError as error:
+        print(f"Connection error: {error}")
 
 if __name__ == "__main__":
     CLIENT_SERVER = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
     CLIENT_SERVER.connect(ADDRESS)
     client_introduce(CLIENT_SERVER)
-    message_thread = threading.Thread(target=send_messages, args=CLIENT_SERVER)
-    receive_message_thread = threading.Thread(target=receive_messages, args=CLIENT_SERVER)
-    receive_message_thread.start()
+
+    message_thread = threading.Thread(target=receive_messages, args=(CLIENT_SERVER,))
     message_thread.start()
+
+    while True:
+        client_message = input("Enter a message you'd like to send: ")
+        CLIENT_SERVER.send(client_message.encode(FORMAT))
+        if client_message.lower() == 'close':
+            CLIENT_SERVER.close()
+            break
+    message_thread.join()
